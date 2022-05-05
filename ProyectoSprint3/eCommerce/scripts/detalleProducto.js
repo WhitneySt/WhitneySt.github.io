11736961479fdfaab858350d4e69631673713ebc
WhitneySt <whitneystena418@gmail.com>
2022-05-05 02:10:47 -0500

    arreglando pagina de agregar producto

--- ProyectoSprint3/eCommerce/scripts/detalleProducto.js
+++ ProyectoSprint3/eCommerce/scripts/detalleProducto.js
@@ -4,59 +4,60 @@ import getData from "https://whitneyst.github.io/ProyectoSprint3/eCommerce/helpe
 
 let idProduct = JSON.parse(localStorage.getItem('verDetalle'));
 
-async function details(id){
-    const url ="https://productos-sprint3.herokuapp.com/productos";
-    const products =  await getData(url);
+async function details(id) {
+    const url = "https://productos-sprint3.herokuapp.com/productos";
+    const products = await getData(url);
     const seleccion = products.find(item => item.id === id);
     const containerDetail = document.querySelector('.detalle');
 
-    const {imagen, productname, precio, descripcion} = seleccion;
+    const { imagen, productname, precio, descripcion } = seleccion;
 
-    containerDetail.innerHTML =`<div class="imagen">
+    containerDetail.innerHTML = `<div class="imagen" id="${idProduct}">
                                     <img src="${imagen}" alt="${productname}">
                                 </div>
                                 <div>
                                     <h1>${productname}</h1>
                                     <h2>$${parseInt(precio).toLocaleString()}</h2>
                                     <p>${descripcion}</p>
-                                    <div class="agregar"><input id="cant" type="number" required><button id="agregarCarro">Agregar al carrito</button></div>
+                                    <div class="agregar"><input id="cant" type="number" required><button class="btnAgregar">Agregar al carrito</button></div>
                                 </div>`
 
 }
 
 details(idProduct);
 //En el evento de click en el boton 'Agregar al carrito' crear el objeto del producto a comprar y añadirlo
-const btnAgregarCarro = document.getElementById('agregarCarro');
-btnAgregarCarro.addEventListener('click', async ()=>{
+const agregarCarro = document.getElementById('agregarCarro');
+agregarCarro.addEventListener('click', async ({ target }) => {
 
-    let url = `https://productos-sprint3.herokuapp.com/productos/${idProduct}`
-    let infoDetalle = await getData(url);
+    if(target.classList.contains('btnAgregar')){
+        let url = `https://productos-sprint3.herokuapp.com/productos/${idProduct}`
+        let infoDetalle = await getData(url);
 
-    const {id, productname, imagen, precio} = infoDetalle;
-    
-    let cant = document.getElementById('cant').value;
-    let subTotal = cant*parseInt(precio);
+        const { id, productname, imagen, precio } = infoDetalle;
 
-    let objCarrito = {
-        id: id,
-        name: productname,
-        img: imagen,
-        precioP: precio,
-        cantidad: cant,
-        sTotal: subTotal
-    }
+        let cant = document.getElementById('cant').value;
+        let subTotal = cant * parseInt(precio);
 
-    fetch('https://productos-sprint3.herokuapp.com/carrito', {
-        method: 'POST',
-        body: JSON.stringify(objCarrito),
-        headers: {
-            "Content-type": "application/json; charset=UTF-8"
+        let objCarrito = {
+            id: id,
+            name: productname,
+            img: imagen,
+            precioP: precio,
+            cantidad: cant,
+            sTotal: subTotal
         }
-   }).then( ()=> {
-      alert("información guardada")
-   })
-   .catch(() => {
-       alert("Hubo un error")
-   })
-    
+
+        fetch('https://productos-sprint3.herokuapp.com/carrito', {
+            method: 'POST',
+            body: JSON.stringify(objCarrito),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        }).then(() => {
+            alert("información guardada")
+        })
+            .catch(() => {
+                alert("Hubo un error")
+            })
+    }
 })
